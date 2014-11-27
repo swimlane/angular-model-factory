c9daf4647466ac25c376885576a0e0f4d67264b7
Juri <juri.strumpflohner@gmail.com>
2014-11-27 10:02:25 +0100

    adds karma test setup with some failing/succeeding tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('ng-annotate');
+    grunt.loadNpmTasks('grunt-karma');
 
     var ngAnnotate = require("ng-annotate");
 
@@ -63,6 +64,15 @@ module.exports = function(grunt) {
               'dist/<%= pkg.name %>.min.js': 'dist/<%= pkg.name %>.js',
               'dist/<%= pkg.name %>-bundle.min.js': 'dist/<%= pkg.name %>-bundle.js'
             }
+          }
+        },
+        karma: {
+          ci: {
+            configFile: 'test/karma.conf.js',
+            singleRun: true,
+            autoWatch: false,
+            browsers: ['PhantomJS'],
+            reporters: ['mocha']
           }
         }
     });
---
+++ test/spec/sample.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('A sample test', function(){
+
+    it('should fail when the tests fail', function(){
+        expect(false).toBeTruthy();
+    });
+
+    it('should succeed when the test succeeds', function(){
+        expect(true).toBeTruthy();
+    });
+
+});
