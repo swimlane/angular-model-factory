0293495f21524f9396f772f31ea62246bc1a5da3
Juri <juri.strumpflohner@gmail.com>
2015-03-11 08:42:54 +0100

    :art: fix lint errors

--- src/modelFactory.js
+++ src/modelFactory.js
@@ -254,7 +254,7 @@ module.provider('$modelFactory', function(){
         list: {}
     };
 
-    provider.$get = ["$http", "$q", "$log", "$cacheFactory", function($http, $q, $log, $cacheFactory) {
+    provider.$get = ['$http', '$q', '$log', '$cacheFactory', function($http, $q, $log, $cacheFactory) {
 
         /**
          * Model factory.
@@ -515,7 +515,7 @@ module.provider('$modelFactory', function(){
                 // Create a copy of the value last so we get all the goodies,
                 // like default values and whatnot.
                 instance.$commit();
-            };
+            }
 
             //
             // Model Static
@@ -569,11 +569,11 @@ module.provider('$modelFactory', function(){
                     }
 
                     // attach the pk referece by default if it is a 'core' type
-                    if(action === "get" || action === "post" || action === "update" || action === "delete"){
-                        uri += "/{" + options.pk + "}";
+                    if(action === 'get' || action === 'post' || action === 'update' || action === 'delete'){
+                        uri += '/{' + options.pk + '}';
                     }
 
-                    if(clone.method === "GET" && (angular.isString(data) || angular.isNumber(data))){
+                    if(clone.method === 'GET' && (angular.isString(data) || angular.isNumber(data))){
                         // if we have a get method and its a number or a string
                         // you can assume i'm wanting to do something like:
                         // ZooModel.get(1234) instead of ZooModel.get({ id: 1234 });
@@ -585,14 +585,14 @@ module.provider('$modelFactory', function(){
                         //
                         if(extras){
                             data.param = extras;
-                            uri += "{?param*}";
+                            uri += '{?param*}';
                         }
-                    } else if(clone.method === "GET" && angular.isObject(data)){
+                    } else if(clone.method === 'GET' && angular.isObject(data)){
                         // if its a GET request and its not the above, we can assume
                         // you want to do a query param like:
                         // ZooModel.query({ type: 'panda' }) and do /api/zoo?type=panda
                         data = { param: data };
-                        uri += "{?param*}";
+                        uri += '{?param*}';
                     }
                 } else {
                     uri = clone.url;
@@ -710,7 +710,7 @@ module.provider('$modelFactory', function(){
             Model.$strip = function(args){
                 // todo: this needs to account for relationships too?
                 // either make recursive or chain invoked
-                if(args && typeof args === "object"){
+                if(args && typeof args === 'object'){
                     forEach(args, function(v,k){
                         if(instanceKeywords.indexOf(k) > -1){
                             delete args[k];
@@ -731,7 +731,7 @@ module.provider('$modelFactory', function(){
             Model.List = ModelCollection;
 
             return Model;
-        };
+        }
 
         return modelFactory;
     }];
